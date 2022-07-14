e4646a90e6155c5d29d1853b130226fee911377c
Thales Menezes <thlmenezes@pm.me>
2022-07-14 19:21:28 -0300

    🔧 (eslint): typescript file parsing

--- packages/eslint-config-acme/index.js
+++ packages/eslint-config-acme/index.js
@@ -1,5 +1,14 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    "next",
+    "prettier"
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
