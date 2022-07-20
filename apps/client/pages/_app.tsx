f83ef14b0f44a2f306416fd117aeb434a118308b
Thales Menezes <thlmenezes@pm.me>
2022-07-20 18:44:01 -0300

    🔧 (client): path alias
    https://nextjs.org/docs/advanced-features/module-path-aliases

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import '@root/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
