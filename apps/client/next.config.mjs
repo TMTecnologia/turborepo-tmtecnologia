5f15f003bccb98ac7c24f32e29be94cd86bee8c4
Thales Menezes <thlmenezes@pm.me>
2022-07-21 14:17:36 -0300

    ✨ (client): validate env on build

---
+++ apps/client/next.config.mjs
@@ -0,0 +1,17 @@
+import { env as _env } from './server/env.mjs';
+
+/**
+ * Use generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+
+export default defineNextConfig({
+  reactStrictMode: true,
+});
