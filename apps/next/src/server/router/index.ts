b5f9828ab2b25830633b53f25bfaf095dbdf8df2
Thales Menezes <thlmenezes@pm.me>
2022-07-21 19:21:41 -0300

    ✨ yarn create t3-app

---
+++ apps/next/src/server/router/index.ts
@@ -0,0 +1,14 @@
+// src/server/router/index.ts
+import superjson from 'superjson';
+
+import { authRouter } from './auth';
+import { createRouter } from './context';
+import { exampleRouter } from './example';
+
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge('example.', exampleRouter)
+  .merge('auth.', authRouter);
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
