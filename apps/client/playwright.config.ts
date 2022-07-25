c9de2bcb6a22d3ee143e0b1675e32b3ad9eda8de
Thales Menezes <thlmenezes@pm.me>
2022-07-25 18:23:34 -0300

    🔧 (client): playwright launch test server
    https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -1,6 +1,12 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'yarn start',
+    url: 'http://localhost:3000/',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
