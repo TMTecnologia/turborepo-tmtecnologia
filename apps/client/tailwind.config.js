056eea3e33c01d004e0f9e32d208301c1b24f142
Thales Menezes <thlmenezes@pm.me>
2022-08-04 02:35:21 -0300

    🔧 tailwind
    https://blog.makerx.com.au/styling-radix-ui-components-using-tailwind-css/

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+let plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
@@ -5,5 +8,46 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function (helpers) {
+      // variants that help styling Radix-UI components
+      dataStateVariant('open', helpers);
+      dataStateVariant('closed', helpers);
+      dataStateVariant('on', helpers);
+      dataStateVariant('checked', helpers);
+      dataStateVariant('unchecked', helpers);
+    }),
+  ],
 };
+
+function dataStateVariant(
+  state,
+  {
+    addVariant, // for registering custom variants
+    e, // for manually escaping strings meant to use in class names
+  }
+) {
+  addVariant(`data-state-${state}`, ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.${e(
+        `data-state-${state}${separator}${className}`
+      )}[data-state='${state}']`;
+    });
+  });
+
+  addVariant(`group-data-state-${state}`, ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.group[data-state='${state}'] .${e(
+        `group-data-state-${state}${separator}${className}`
+      )}`;
+    });
+  });
+
+  addVariant(`peer-data-state-${state}`, ({ modifySelectors, separator }) => {
+    modifySelectors(({ className }) => {
+      return `.peer[data-state='${state}'] ~ .${e(
+        `peer-data-state-${state}${separator}${className}`
+      )}`;
+    });
+  });
+}
