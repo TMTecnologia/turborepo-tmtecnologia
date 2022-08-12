9fee4812b89c562afe6ee34e959f91c3b59985b1
Thales Menezes <thlmenezes@pm.me>
2022-08-12 10:19:08 -0300

    👔 🔥 (danger): update test policy

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,4 @@
-import { danger, message, schedule, warn } from 'danger';
+import { danger, message, schedule } from 'danger';
 import * as reporter from 'danger-plugin-lint-report';
 import todos from 'danger-plugin-todos';
 import yarn from 'danger-plugin-yarn';
@@ -14,20 +14,11 @@ import {
 } from './.danger';
 
 const docs = danger.git.fileMatch('**/*.md');
-const app = danger.git.fileMatch('(apps|packages)/**/*.ts*');
-const tests = danger.git.fileMatch('**/(*.)+(spec|test).ts');
 
 if (docs.edited) {
   message(
     'Thanks - We :heart: our [documentarians](http://www.writethedocs.org/)!'
   );
-}
-
-if (app.modified && !tests.modified) {
-  const message =
-    'There are app changes, but not tests.' +
-    "That's OK as long as you're refactoring existing code";
-  warn(message);
 }
 
 yarn();
