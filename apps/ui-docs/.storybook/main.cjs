bc9c356c08aee133a80bc634008d8eeb5f245ecc
Thales Menezes <thlmenezes@pm.me>
2022-08-16 22:21:55 -0300

    ♻️ 🔥 (ui-docs): focus on stories

--- apps/ui-docs/.storybook/main.cjs
+++ apps/ui-docs/.storybook/main.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-essentials",
