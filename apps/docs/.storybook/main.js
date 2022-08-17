539f2f361b905841c91f5b6d36aa07f4f45a7f3d
Thales Menezes <thlmenezes@pm.me>
2022-08-16 23:08:59 -0300

    ✨ (docs): validate a11y on stories

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-vite',
